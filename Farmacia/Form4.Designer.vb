910ee227b19abd0db0829d654726983d94a3c39f
JESSURI CRISTAL BAEZA CRUZ <19052596.baeza@ms.uas.edu.mx>
2023-05-22 22:03:57 -0700

    pp

--- Farmacia/Form4.Designer.vb
+++ Farmacia/Form4.Designer.vb
@@ -22,30 +22,38 @@ Partial Class Form4
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim Panel4 As System.Windows.Forms.Panel
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
         Me.Panel3 = New System.Windows.Forms.Panel()
         Me.Panel2 = New System.Windows.Forms.Panel()
-        Panel4 = New System.Windows.Forms.Panel()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Panel1
         '
         Me.Panel1.BackgroundImage = Global.Farmacia.My.Resources.Resources.lado
-        Me.Panel1.Controls.Add(Panel4)
+        Me.Panel1.Controls.Add(Me.Panel4)
         Me.Panel1.Controls.Add(Me.Panel3)
         Me.Panel1.Controls.Add(Me.Panel2)
         Me.Panel1.Location = New System.Drawing.Point(1, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(150, 453)
+        Me.Panel1.Size = New System.Drawing.Size(178, 556)
         Me.Panel1.TabIndex = 0
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.Transparent
+        Me.Panel4.BackgroundImage = Global.Farmacia.My.Resources.Resources.pais__1_
+        Me.Panel4.Location = New System.Drawing.Point(34, 245)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(82, 87)
+        Me.Panel4.TabIndex = 2
         '
         'Panel3
         '
         Me.Panel3.BackColor = System.Drawing.Color.Transparent
         Me.Panel3.BackgroundImage = Global.Farmacia.My.Resources.Resources.modelo1
-        Me.Panel3.Location = New System.Drawing.Point(34, 333)
+        Me.Panel3.Location = New System.Drawing.Point(34, 413)
         Me.Panel3.Name = "Panel3"
         Me.Panel3.Size = New System.Drawing.Size(83, 81)
         Me.Panel3.TabIndex = 1
@@ -54,26 +62,17 @@ Partial Class Form4
         '
         Me.Panel2.BackColor = System.Drawing.Color.Transparent
         Me.Panel2.BackgroundImage = Global.Farmacia.My.Resources.Resources.marca1
-        Me.Panel2.Location = New System.Drawing.Point(34, 68)
+        Me.Panel2.Location = New System.Drawing.Point(34, 67)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(83, 82)
+        Me.Panel2.Size = New System.Drawing.Size(82, 83)
         Me.Panel2.TabIndex = 0
-        '
-        'Panel4
-        '
-        Panel4.BackColor = System.Drawing.Color.Transparent
-        Panel4.BackgroundImage = Global.Farmacia.My.Resources.Resources.pais__1_
-        Panel4.Location = New System.Drawing.Point(34, 185)
-        Panel4.Name = "Panel4"
-        Panel4.Size = New System.Drawing.Size(82, 82)
-        Panel4.TabIndex = 3
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.Farmacia.My.Resources.Resources.fondo_4
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1169, 545)
         Me.Controls.Add(Me.Panel1)
         Me.ForeColor = System.Drawing.SystemColors.ControlLight
         Me.Name = "Form4"
@@ -84,8 +83,9 @@ Partial Class Form4
 
     End Sub
 
-
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Panel3 As Panel
     Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel4 As Panel
 End Class
+
--- Farmacia/Form4.vb
+++ Farmacia/Form4.vb
@@ -1,5 +1,6 @@
 ﻿<DebuggerDisplay("{GetDebuggerDisplay(),nq}")>
 Public Class Form4
+    Public Property Panel5 As Object
 
     Private Sub Panel2_MouseHover(sender As Object, e As EventArgs) Handles Panel2.MouseHover
         Panel2.Size = New Size(85, 84)
@@ -9,4 +10,18 @@ Public Class Form4
         Panel2.Size = New Size(83, 82)
     End Sub
 
+    Private Sub Panel3_MouseHover(sender As Object, e As EventArgs) Handles Panel3.MouseHover
+        Panel3.Size = New Size(86, 84)
+    End Sub
+
+    Private Sub Panel3_MouseLeave(sender As Object, e As EventArgs) Handles Panel3.MouseLeave
+        Panel3.Size = New Size(83, 81)
+    End Sub
+    Private Sub Panel4_MouseHover(sender As Object, e As EventArgs) Handles Panel4.MouseHover
+        Panel4.Size = New Size(85, 90)
+    End Sub
+
+    Private Sub Panel4_MouseLeave(sender As Object, e As EventArgs) Handles Panel4.MouseLeave
+        Panel4.Size = New Size(82, 87)
+    End Sub
 End Class
