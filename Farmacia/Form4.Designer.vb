e83d9242c2d4e4596f3f0bb6a337b1d40095c7b9
JESSURI CRISTAL BAEZA CRUZ <19052596.baeza@ms.uas.edu.mx>
2023-05-22 20:44:34 -0700

    agregue un form

--- Farmacia/Form4.Designer.vb
+++ Farmacia/Form4.Designer.vb
@@ -22,9 +22,17 @@ Partial Class Form4
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'Form4
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLight
+        Me.Name = "Form4"
         Me.Text = "Form4"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
