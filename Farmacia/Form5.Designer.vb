d78a808ea6a59b666d82a0c0f706559ce7603cde
JESSURI CRISTAL BAEZA CRUZ <19052596.baeza@ms.uas.edu.mx>
2023-05-24 22:02:22 -0700

    form 5

--- Farmacia/Form5.Designer.vb
+++ Farmacia/Form5.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form5
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class form5
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,18 +20,127 @@ Partial Class Form5
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(t:=GetType(form5))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.botton_closed = New System.Windows.Forms.FlowLayoutPanel()
+        Me.botton_minimized = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Form5
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Navy
+        Me.Panel1.Controls.Add(Me.botton_closed)
+        Me.Panel1.Controls.Add(Me.botton_minimized)
+        Me.Panel1.Location = New System.Drawing.Point(-7, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(936, 29)
+        Me.Panel1.TabIndex = 0
+        '
+        'botton_closed
+        '
+        Me.botton_closed.BackColor = System.Drawing.Color.Transparent
+        Me.botton_closed.BackgroundImage = CType(resources.GetObject("botton_closed.BackgroundImage"), System.Drawing.Image)
+        Me.botton_closed.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.botton_closed.Location = New System.Drawing.Point(868, -7)
+        Me.botton_closed.Name = "botton_closed"
+        Me.botton_closed.Size = New System.Drawing.Size(42, 41)
+        Me.botton_closed.TabIndex = 1
+        '
+        'botton_minimized
+        '
+        Me.botton_minimized.BackColor = System.Drawing.Color.Transparent
+        Me.botton_minimized.BackgroundImage = CType(resources.GetObject("botton_minimized.BackgroundImage"), System.Drawing.Image)
+        Me.botton_minimized.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.botton_minimized.Location = New System.Drawing.Point(834, -8)
+        Me.botton_minimized.Name = "botton_minimized"
+        Me.botton_minimized.Size = New System.Drawing.Size(43, 37)
+        Me.botton_minimized.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(276, 150)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 21)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Usuario:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(276, 208)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(107, 21)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Contraseña:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(280, 174)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(198, 20)
+        Me.TextBox1.TabIndex = 6
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(280, 249)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(198, 20)
+        Me.TextBox2.TabIndex = 7
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.BackColor = System.Drawing.Color.Transparent
+        Me.LinkLabel1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LinkLabel1.Location = New System.Drawing.Point(285, 414)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(193, 21)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "¿Olvido su contraseña?"
+        '
+        'form5
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Name = "Form5"
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(905, 537)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "form5"
+        Me.Opacity = 0.9R
         Me.Text = "Form5"
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents botton_closed As FlowLayoutPanel
+    Friend WithEvents botton_minimized As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
